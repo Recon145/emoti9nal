382a441dcbdb9b8ce58d25aeed20526e3c3aa405
Recon145 <reconmovyie@gmail.com>
2017-11-25 22:45:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var io = require('socket.io')(http);
  
 io.on('connection',function(socket){
     console.log('Yaşasın Soket Bağlantısı Başarılı !');
-    socket.emit('baglanincaHaberVer');
-    socket.on('baglantiBasarili',function(){
-        console.log('Karşılıklı bağlantı başarıyla sağlandı.');
-    });
+    //socket.emit('baglanincaHaberVer');
+    //socket.on('baglantiBasarili',function(){
+       // console.log('Karşılıklı bağlantı başarıyla sağlandı.');
+   // });
 });
