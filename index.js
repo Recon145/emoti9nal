3a63005771d2fcf09091346734e256fa5bda6500
Recon145 <reconmovyie@gmail.com>
2017-11-25 22:11:05 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var io = require('socket.io').listen(3000); //3000 portunu dinlemeye başladık.
-    io.sockets.on('connection', function(socket){ // tüm node işlemlerini kapsayan ana fonksiyonumuz
-        socket.on('mesajgonder', function(data){ //clientte'ki mesajı aldık
-            socket.emit('mesajgitti', data) //server mesajı client'e geri gönderdi emit ile
-            socket.broadcast.emit('mesajgitti', data) //
-        });
-});
