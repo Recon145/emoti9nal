320a640f46bd76ad5442e17d3b64980b1c7cf90a
Recon145 <reconmovyie@gmail.com>
2017-11-25 22:48:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var app = require('express')();
-var http = require('http').Server(app);
+//var app = require('express')();
+//var http = require('http').Server(app);
 ///var io = require('socket.io')(http);
  
 ///io.on('connection',function(socket){
