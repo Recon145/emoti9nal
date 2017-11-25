9b80f2a7f118c68253bb442301e037dd44bcda60
Recon145 <reconmovyie@gmail.com>
2017-11-25 22:47:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+///var io = require('socket.io')(http);
  
-io.on('connection',function(socket){
-    console.log('Yaşasın Soket Bağlantısı Başarılı !');
+///io.on('connection',function(socket){
+  //  console.log('Yaşasın Soket Bağlantısı Başarılı !');
     //socket.emit('baglanincaHaberVer');
     //socket.on('baglantiBasarili',function(){
        // console.log('Karşılıklı bağlantı başarıyla sağlandı.');
    // });
-});
+//});
